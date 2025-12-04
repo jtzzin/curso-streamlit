3ca59df32d24803291fae1f5086d147076295767
Juan Thales <juanthalesap@gmail.com>
2025-12-04 18:59:53 -0300

    primeiro commit

---
+++ aulas/1.py
@@ -0,0 +1,10 @@
+import streamlit as st;
+
+st.title("Aula sobre botoes e side bar");
+
+st.header("veremos como criar uma barra lateral no site");
+
+# side bar
+st.sidebar.header("aqui, comecamos com a sidebar de fato");
+
+# teste
